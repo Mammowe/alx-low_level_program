74a085fc6e976913a79dc4a45e54481f0d7f0014
Mammowe <kgoputsolerato@yahoo.com>
2023-02-20 06:45:34 -0800

    Lowercase.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-/** main - Description: to print letter in lowercase
- *
- * Return: Always 0 (Success)
- *
- */
-
+/**
+* main - entry point
+*
+* Description: print letters in lowercase
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char lcl +'a';
-	for (lcl = 'a'; lcl<='z'; lcl++)
-	{
-		putchar(lcl);
-	}
-	putchar('\n');
-	return (0);
+char lcl = 'a';
+
+for (lcl = 'a'; lcl <= 'z'; lcl++)
+{
+putchar(lcl);
+}
+putchar('\n');
+return (0);
 }
 
 
