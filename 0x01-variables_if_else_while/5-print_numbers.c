84945f0cb919fe04dbc0c89290772c328cdf328f
Mammowe <kgoputsolerato@yahoo.com>
2023-02-20 07:45:56 -0800

    Numbers.

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,23 +3,22 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- *
- * Desciption: print all single base 10 numbers
- *
- * Return: Always 0 (Success)
- *
- */
+* main - Entry point
+*
+* Description:print all single base 10 numbers
+*
+* Return: Always 0 (Success)
+*
+*/
 
 int main(void)
 
 {
 int n = 0;
 
-for (n=0; n<=9; n++)
-
+for (n = 0; n <= 9; n++)
 {
-printf("%d");
+printf("%d", n);
 
 }
 printf("\n");
