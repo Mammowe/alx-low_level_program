0a494bd8a5b5ec103ecc679f59e053ea5ba6cb24
Mammowe <kgoputsolerato@yahoo.com>
2023-02-20 10:53:36 -0800

    Tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,24 +2,24 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- *
- * Description: print letters in lowercase in reverse
- *
- * Return: Always 0 (Success)
- *
- */
+* main - entry point
+*
+* Description: print letters in lowercase in reverse
+*
+* Return: Always 0 (Success)
+*/
+
 
 int main(void)
 {
 char lcl = 'z';
 
-for (lcl = 'z'; lcl = 'a'; lcl--)
+for (lcl = 'z'; lcl >= 'a'; lcl--)
 {
 putchar(lcl);
-
 }
-putchar ('\n');
+
+putchar('\n');
 
 return (0);
 }
