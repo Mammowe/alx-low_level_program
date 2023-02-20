a9314881c76ec78c697efcb5e10cc8eccded01c4
Mammowe <kgoputsolerato@yahoo.com>
2023-02-20 07:39:12 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *
+ * main - entry point
+ *
+ * Desciption: print all single base 10 numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n = 0;
+for (n=0; n<=9; n++)
+{
+printf("%d");
+}
+printf("\n");
+return (0);
+}
