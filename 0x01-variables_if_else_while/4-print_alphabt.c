92850127842b2074388ae70bcd7560b1f696eb59
Mammowe <kgoputsolerato@yahoo.com>
2023-02-20 07:07:27 -0800

    Lower without e,q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdoi.h>
+
+/**
+* main - Entry point
+*
+* Description:print letters in lowercase except q and e
+*
+* Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+
+{
+	char lcl = 'a';
+
+	for (lcl = 'a'; lcl <= 'z'; lcl++)
+	{
+		if (lcl != 'q' && lcl != 'e')
+		{
+			putchar(lcl);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
