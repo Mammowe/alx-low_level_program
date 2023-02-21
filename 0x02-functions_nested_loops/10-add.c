5c678aecd19c6441f19442e8be2589c98b00857d
Mammowe <kgoputsolerato@yahoo.com>
2023-02-21 13:11:20 -0800

    Sum of two

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
-
 /**
- * add - entry point
- * 
- * @i: will be the first interger
- * @j: will be the second interger
- *
- * Description: take two intergers add the together
- *
- * Return: the sum of two intergers
- *
- */
+*add- entry point
+*
+* @i:will be the first interger
+* @j:the second intergert
+*
+*description: 'take two intergers and add them together'
+*
+*Return:the sum of the 2 intergers
+*/
 
 int add(int i, int j)
 {
-	
-	return ((i+j));
 
+	return ((i + j));
 
 }
 
