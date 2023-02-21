92a150fcb5b656938c4cf4b5eee0124bded6910a
Mammowe <kgoputsolerato@yahoo.com>
2023-02-21 05:27:08 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: function that prints _putchar
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar");
+
+	printf("\n");
+
+	return (0);
+}
