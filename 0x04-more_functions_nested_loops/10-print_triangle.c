eb55139e301407b0dfc3a4e9a68e9665a9576263
Mammowe <kgoputsolerato@yahoo.com>
2023-02-24 10:55:03 -0800

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+*print_triangle- entry point
+*
+*@size: input variable
+*
+*description: 'print a triangle with #'
+*
+*return:# in a shape of triangle
+*/
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+
+}
+
+
