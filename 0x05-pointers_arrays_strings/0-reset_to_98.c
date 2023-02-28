b6ab700ea45da4fe2d0cfb8a502b5e744aa40b65
Mammowe <kgoputsolerato@yahoo.com>
2023-02-28 01:41:11 -0800

    98 battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reset_to_98- entry point
+ *
+ * @n: input variable
+ *
+ * Description:  takes a pointer to an int as parameter and
+ *  updates the value it points to to 98
+ *
+ * Return : n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
+
