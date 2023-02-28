550c8dfde14a84765a0c1942acf2c24f2754b8f9
Mammowe <kgoputsolerato@yahoo.com>
2023-02-28 02:38:26 -0800

    copy from src to dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,13 +13,20 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int i =0;
+	int j =0;
 
-	for (i = 0; src[i] != '\0'; i++)
+	while (*(src + 1) != '\0')
 	{
-		dest[i] =  src[i];
-
+		i++;
 	}
+
+	for (; j < 1; j++)
+	{
+		dest[j] = src[j];
+	}
+
+	dest[i] = '\0';
 	return (dest);
 }
 
