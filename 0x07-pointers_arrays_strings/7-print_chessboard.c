667f02444a05aec0270810248edf88886ebdd806
Mammowe <kgoputsolerato@yahoo.com>
2023-03-06 08:53:32 -0800

    Chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,28 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * print_chessboard - Entry point
- * @a: array
- * Return: Always 0 (Success)
- */
+*print_chessboard- Entry point
+*
+*Description: prints a chessboard
+*@a: input variable
+*
+*Return:
+*/
+
 void print_chessboard(char (*a)[8])
 {
-	int i;
-	int j;
+	int d, c;
 
-	for (i = 0; i < 8; i++)
+	for (d = 0; d <= 8; d++)
 	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-		_putchar('\n');
+		for (c = 0; c <= 8; c++)
+		{
+			_putchar(a[d][c]);
+		}
+		_putchar(10);
 	}
+
 }
 
 
