bf6e8743983396f3fa244abfe487d2b7ca2b2535
Mammowe <kgoputsolerato@yahoo.com>
2023-03-31 16:41:20 -0700

    List length

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,22 +6,21 @@
 #include <string.h>
 
 /**
- * struct lists_s - singly linked list
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
- * @next: point to the next node
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
  * for Holberton project
  */
-
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
 } list_t;
-int putchar(char c);
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
