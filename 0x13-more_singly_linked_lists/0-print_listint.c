ab06f1b84a5ddfb91019d6e69701b99485278da0
Mammowe <kgoputsolerato@yahoo.com>
2023-04-03 15:09:16 -0700

    Print listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * print_listint -entry point
+ * @h: input
+ * @ Returb: list
+ *
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint *ptr;
+	size_t i = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	prt = h;
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		i++;
+		ptr = ptr -> next;
+	}
+	return(i);
+}
+
+
+
